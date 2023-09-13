1829db777e8a3deb9e6d68d62543edec6a86acfc
Eduardo-Malhao <eduardo.malhao@outlook.com>
2023-09-13 17:03:43 -0300

    REQ 03

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,28 @@
-# Req 3
+import csv
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.source_path = source_path
+        self.dishes = self._load_data()
+
+    def _load_data(self):
+        dishes = set()
+        temporary = {}
+
+        with open(self.source_path) as file:
+            reader = csv.reader(file)
+            for row in reader:
+                name = row["dish"]
+                price = float(row["price"])
+                ingredient = row["ingredient"]
+                amount = int(row["recipe_amount"])
+
+                if name not in temporary:
+                    temporary[name] = Dish(name, price)
+                temporary[name].add_ingredient_dependency(Ingredient(ingredient), amount)
+                dishes.add(temporary[name])
+
+        return dishes
